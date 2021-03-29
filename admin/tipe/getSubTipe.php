fffa7047b7e3eac4670e9694acb60e178d89aba0
rian <rianwahyu26@gmail.com>
2021-03-29 11:19:02 +0700

    nested array php jenis kendaraan

---
+++ admin/tipe/getSubTipe.php
@@ -0,0 +1,56 @@
+<?php
+
+include '../../connection.php';
+
+class data{}
+
+
+$subTipeArray = array();
+$subTipeHargaArray = array();
+$myArray = array();
+
+$query = "SELECT * FROM jenisKendaraan";
+$result = mysqli_query($dbc, $query);
+if (mysqli_num_rows($result) >= 1) {
+
+    while($data = mysqli_fetch_array($result)){
+        
+    
+        $myArray[$data['idJenis']]= array('idJenis' => $data['idJenis'],
+				'namaJenis' => $data['namaJenis'],				
+				'listHarga' => array());
+
+
+        $querys = "SELECT * FROM jenisharga WHERE idJenis ='$data[idJenis]' ";
+        $results = mysqli_query($dbc, $querys);
+        while($datas = mysqli_fetch_array($results)){
+
+            $idHarga = $datas['idHarga'];
+            $keterangan = $datas['keterangan'];
+            $harga = $datas['harga'];
+
+            $subTipeHargaArray = array("idHarga" => $idHarga,
+            "keterangan" => $keterangan, 
+            "harga" => $harga);
+            
+            $myArray[$datas['idJenis']]['listHarga'][]= array(
+                'idHarga' => $datas['idHarga'],
+                'keterangan' => $datas['keterangan'],
+                'harga' => $datas['harga']);
+        }
+        
+    }
+
+    $response = new data();
+    $response->success = TRUE;
+    $response->message = "Berhasil Mendapatkan Data";
+    $response->data = $arrays = array_values($myArray);;
+    die(json_encode($response));
+}else{
+    $response = new data();
+    $response->success = FALSE;
+    $response->message = "Tidak ada Data";
+    die(json_encode($response));
+}
+
+mysqli_close($dbc);
