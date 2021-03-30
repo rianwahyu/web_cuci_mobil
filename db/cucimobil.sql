c271ea5ba5f2788583cde8783b146d17b69f5731
rian <rianwahyu26@gmail.com>
2021-03-30 16:44:24 +0700

    booking get kategori kendaraan

--- db/cucimobil.sql
+++ db/cucimobil.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 29, 2021 at 12:10 PM
+-- Generation Time: Mar 30, 2021 at 11:43 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.3.23
 
@@ -63,6 +63,27 @@ CREATE TABLE `banner` (
 
 INSERT INTO `banner` (`idBanner`, `namaBanner`, `file`, `status`) VALUES
 (4, 'testing', 'ed02f7074cf1140f0ff35815c3f68031.png', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booking`
+--
+
+CREATE TABLE `booking` (
+  `orderID` varchar(10) NOT NULL,
+  `userID` varchar(50) NOT NULL,
+  `tipeKendaraan` varchar(200) NOT NULL,
+  `idKategori` int(11) NOT NULL,
+  `idJenis` int(11) NOT NULL,
+  `idHarga` int(11) NOT NULL,
+  `alamatOrder` varchar(200) NOT NULL,
+  `latitude` varchar(100) NOT NULL,
+  `longitude` varchar(100) NOT NULL,
+  `tanggalOrder` date NOT NULL,
+  `waktuOrder` time NOT NULL,
+  `statusOrder` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -134,15 +155,40 @@ INSERT INTO `kategorikendaraan` (`idKategori`, `namaKategori`, `foto`) VALUES
 --
 
 CREATE TABLE `member` (
-  `idMember` varchar(10) NOT NULL,
+  `idMember` varchar(50) NOT NULL,
   `namaLengkap` varchar(100) NOT NULL,
-  `alamat` varchar(200) NOT NULL,
+  `alamat` varchar(200) NOT NULL DEFAULT ' ',
   `noHp` varchar(13) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `token` text NOT NULL,
+  `token` text NOT NULL DEFAULT ' ',
   `tanggalRegistrasi` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `member`
+--
+
+INSERT INTO `member` (`idMember`, `namaLengkap`, `alamat`, `noHp`, `email`, `password`, `token`, `tanggalRegistrasi`) VALUES
+('XEZXSSv2r0bbCu1FIrdOvrFoMCA3', 'Rian Wahyu', ' ', '', 'riansahadah@gmail.com', '256fa07ec1bff2c9ce5e104ad65cb36a', ' ', '2021-03-30 09:34:04');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembayaran`
+--
+
+CREATE TABLE `pembayaran` (
+  `idPembayaran` int(11) NOT NULL,
+  `namaPembayaran` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pembayaran`
+--
+
+INSERT INTO `pembayaran` (`idPembayaran`, `namaPembayaran`) VALUES
+(1, 'COD');
 
 --
 -- Indexes for dumped tables
@@ -159,6 +205,12 @@ ALTER TABLE `administrator`
 --
 ALTER TABLE `banner`
   ADD PRIMARY KEY (`idBanner`);
+
+--
+-- Indexes for table `booking`
+--
+ALTER TABLE `booking`
+  ADD PRIMARY KEY (`orderID`);
 
 --
 -- Indexes for table `jenisharga`
@@ -183,6 +235,12 @@ ALTER TABLE `kategorikendaraan`
 --
 ALTER TABLE `member`
   ADD PRIMARY KEY (`idMember`);
+
+--
+-- Indexes for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD PRIMARY KEY (`idPembayaran`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -211,6 +269,12 @@ ALTER TABLE `jeniskendaraan`
 --
 ALTER TABLE `kategorikendaraan`
   MODIFY `idKategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  MODIFY `idPembayaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ member/booking/getHarga.php
@@ -0,0 +1,28 @@
+<?php
+
+include '../../connection.php';
+
+class data{}
+
+$idJenis = $_POST['idJenis'];
+
+$query = "SELECT * FROM jenisharga WHERE idJenis ='$idJenis' ";
+$result = mysqli_query($dbc, $query);
+if (mysqli_num_rows($result) >= 1) {
+    while($data = mysqli_fetch_array($result)){
+        $myArray[]=$data;
+    }
+
+    $response = new data();
+    $response->success = TRUE;
+    $response->message = "Berhasil Mendapatkan Data";
+    $response->data = $myArray;
+    die(json_encode($response));
+}else{
+    $response = new data();
+    $response->success = FALSE;
+    $response->message = "Tidak ada Data";
+    die(json_encode($response));
+}
+
+mysqli_close($dbc);
