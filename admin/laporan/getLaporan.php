b74951b9a430909a6a538fc15a4bdde49d469ec5
rian <rianwahyu26@gmail.com>
2021-04-07 16:12:59 +0700

    laporan

--- admin/laporan/getLaporan.php
+++ admin/laporan/getLaporan.php
@@ -1,21 +1,27 @@
-<?php 
-
-include '../../connection.php';
+<?php
 
 class data{}
 
-$query = "SELECT * FROM `member` WHERE 1 ";
+include '../../connection.php';
+
+$start = $_POST['start']." 00:00:00";
+$end = $_POST['end']. " 23:59:00";
+
+$query="SELECT a.idReport, a.orderID, a.hargaFinal, a.hargaKetFinal, a.tanggalOrder, a.tanggalSelesaiOrder, c.namaLengkap 
+FROM laporan a 
+INNER JOIN booking b ON a.orderID = b.orderID 
+INNER JOIN member c ON b.userID = c.idMember
+WHERE a.tanggalOrder BETWEEN '$start' AND '$end' ";
+
 $result = mysqli_query($dbc, $query);
 if (mysqli_num_rows($result) >= 1) {
-
     while($data = mysqli_fetch_array($result)){
-        $myArray[] = $data;
+        $myArray[]=$data;
     }
-
     $response = new data();
     $response->success = TRUE;
     $response->message = "Berhasil Mendapatkan Data";
-    $response->data = $arrays = array_values($myArray);;
+    $response->data = $myArray;
     die(json_encode($response));
 }else{
     $response = new data();
@@ -23,5 +29,3 @@ if (mysqli_num_rows($result) >= 1) {
     $response->message = "Tidak ada Data";
     die(json_encode($response));
 }
-
-mysqli_close($dbc);
